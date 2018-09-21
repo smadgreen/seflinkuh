b6927b2ee468aae098c05e6a40487af393eaaf2b
smadgreen <43455223+smadgreen@users.noreply.github.com>
2018-09-21 22:08:09 +0700

    Update multy.js

--- multy.js
+++ multy.js
@@ -32,3 +32,12 @@ if(a_to_vk)
 {a_to_vk.innerHTML+=a_to_va;}}
 function a_to_fa()
 {var a_to_vf=new Array();protected_links=protected_links.replace(" ","");a_to_vf=protected_links.split(",");return a_to_vf;}
+</script>
+
+<script type="text/javascript">
+
+protected_links = "teknobaubau.blogspot.com,blogspot.com,seflinkuh.blogspot.com,blogger.com,google.com,youtube.com,facebook.com";
+
+auto_safelink();
+
+</script>
