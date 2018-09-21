56c4b36f1e235e702203c5e26a648560ded32e68
smadgreen <43455223+smadgreen@users.noreply.github.com>
2018-09-21 22:00:04 +0700

    Update multy.js

--- multy.js
+++ multy.js
@@ -25,7 +25,7 @@ if(x=="1"){xxx="/2018/09/how-to-get-girls-attention-easily.html"}
 if(x=="2"){xxx="/2018/09/easy-way-to-forget-your-ex-boygirl.html"}
 if(x=="3"){xxx="/2018/09/easy-way-to-gain-weight.html"}
 if(x=="4"){xxx="/2018/09/how-to-speed-up-your-learning-process.html"}
-if(x=="5"){xxx="/2018/09/how-to-ease-anger-easily.html"}a_to_ve[i].href="https://convert-safelink.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="5"){xxx="/2018/09/how-to-ease-anger-easily.html"}a_to_ve[i].href="https://seflinkuh.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
